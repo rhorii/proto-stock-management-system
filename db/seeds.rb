48393161527b1982e05e7c8834584da2a91c53cb
Ryota Horii <rhorii@gmail.com>
2022-01-19 13:35:06 +0900

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+if Rails.env.development?
+  Item.create!(name: 'hoge', description: 'hogehoge', price: 10000, qty: 10)
+  Item.create!(name: 'piyo', description: 'piyopiyo', price: 20000, qty: 20)
+
+  User.create!(email: 'test@example.com', password: 'password', password_confirmation: 'password')
+
+  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+end
